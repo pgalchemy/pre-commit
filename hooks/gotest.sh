49295695a707b304682a4158d009ee2b0313beb5
Joshua Rose <joshuarose@outlook.com>
2019-12-02 13:45:01 -0800

    Syntax changes and exclusions added

--- hooks/gotest.sh
+++ hooks/gotest.sh
@@ -7,6 +7,7 @@ set -e
 # workaround to allow GitHub Desktop to work, add this (hopefully harmless) setting here.
 export PATH=$PATH:/usr/local/bin
 
+
 for file in "$@"; do
-  go test "$(dirname "$file")"
+  go test "./$(dirname "$file")"
 done
